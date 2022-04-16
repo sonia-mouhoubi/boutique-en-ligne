c2d69c2d8323ce5d54e3817551ecbdd57f6f835f
M <sonia.mouhoubi@laplateforme.io>
2022-04-16 08:48:34 +0200

    Page coté front pour afficher les produits

---
+++ views/apresShampoingView.php
@@ -0,0 +1,25 @@
+<?php 
+$title = 'Page produits'; 
+$description = 'Boutique en ligne, shampoing, aprés-shampoing et soins pour tous types de cheveux.'; 
+
+ob_start(); ?>
+
+<section class="produits">
+    <h2>Aprés-shampoing</h2>
+    
+    <?php
+    foreach ($res as $value) { ?>
+    <div>
+        <a href="">
+            <img src="./public/img/<?= $value['image'] ?>" alt="Nom du produit">
+            <h3><?= $value['nom_produit']?></h3>
+        </a>
+        <p><span>Prix TTC </span><?= $value['prixTTC']?> €</p>
+    </div>    
+    <?php } ?>
+</section>
+
+<?php $content = ob_get_clean(); 
+
+require('template.php'); 
+?>
---
+++ views/productsfrizzyView.php
@@ -0,0 +1,22 @@
+<?php 
+$title = 'Page produits'; 
+$description = 'Boutique en ligne, shampoing, aprés-shampoing et soins pour tous types de cheveux.'; 
+
+ob_start(); ?>
+
+<section class="products">
+    <h2>Cheveux crépus</h2>
+
+    <?php foreach ($res as $value) { ?>
+    <div>
+        <img src="./public/img/<?= $value['image'] ?>" alt="Nom du produit">
+        <h3><?= $value['nom_produit']?></h3>
+        <p><span>Prix TTC </span><?= $value['prixTTC']?> €</p>
+    </div>    
+    <?php } ?>
+</section>
+
+<?php $content = ob_get_clean(); 
+
+require('template.php'); 
+?>
